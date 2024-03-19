8bd066834e68df0de59da3e1c5d8d25301d0d4e1
white500 <964142664@qq.com>
2024-03-19 16:04:33 +0800

    feature: HelloPoint2动态传值

--- learn-webgl/Section2/HelloPoint2.js
+++ learn-webgl/Section2/HelloPoint2.js
@@ -1,22 +1,25 @@
-var VSHADER_SOURCE = `
+const VSHADER_SOURCE = `
+attribute vec4 a_Position;
+attribute float a_PointSize;
 void main() {
-    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-    gl_PointSize = 10.0;
+    gl_Position = a_Position;
+    gl_PointSize = a_PointSize;
 }
 `
 
-var FSHADER_SOURCE = `
+const FSHADER_SOURCE = `
 void main() {
     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
 `
 
-
 function main() {
     // 获取<canvas>元素
+    /** @type { HTMLCanvasElement } */
     const canvas = document.getElementById("webgl");
 
     // 获取WebGL绘图上下文
+    /** @type { WebGLRenderingContext } */
     const gl = getWebGLContext(canvas);
     if (!gl) {
         console.log('Failed to get the rendering context for WebGL');
@@ -28,6 +31,17 @@ function main() {
         console.log('Failed to initialize shaders.');
         return;
     }
+
+    const a_Postion = gl.getAttribLocation(gl.program, 'a_Position')
+    const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize')
+
+    if (a_Postion < 0 || a_PointSize < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+
+    gl.vertexAttrib3f(a_Postion, 0.0, 0.0, 0.0)
+    gl.vertexAttrib1f(a_PointSize, 20.0)
 
     // 设置<canvas>的背景色 用齐次坐标表示顶点坐标的时候 只要将最后一个分量赋值为1.0就可以了
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
