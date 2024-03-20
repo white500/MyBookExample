f5bdbff9eb1ca2672227fb2a764febfac5d20ae4
white500 <964142664@qq.com>
2024-03-20 14:02:15 +0800

    添加多个点

---
+++ learn-webgl/Section3/MultPoint.js
@@ -0,0 +1,74 @@
+const VSHADER_SOURCE = `
+attribute vec4 a_Position;
+void main() {
+    gl_Position = a_Position;
+    gl_PointSize = 10.0;
+}
+`
+
+const FSHADER_SOURCE = `
+void main() {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+`
+
+function main() {
+    // 获取<canvas>元素
+    /** @type { HTMLCanvasElement } */
+    const canvas = document.getElementById("webgl");
+
+    // 获取WebGL绘图上下文
+    /** @type { WebGLRenderingContext } */
+    const gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return
+    }
+
+    // 初始化着色器
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.');
+        return;
+    }
+
+    // 设置顶点位置
+    const n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return
+    }
+
+    // 设置<canvas>的背景色 用齐次坐标表示顶点坐标的时候 只要将最后一个分量赋值为1.0就可以了
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // 清空<canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    // 绘制三个点
+    gl.drawArrays(gl.POINTS, 0, n);
+}
+
+function initVertexBuffers(gl) {
+    let vertices = new Float32Array([
+        0.0,0.5,-0.5,-0.5,0.5,-0.5
+    ])
+
+    var n = 3;
+    // 创建缓冲区对象
+    const vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1
+    }
+
+    // 将缓冲去对象绑定到目标
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // 向缓冲区对象中写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    // 将缓冲区对象分配给a_Position变量
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    //连接a_Position变量与分配给它的缓冲区对象
+    gl.enableVertexAttribArray(a_Position);
+    return n;
+}
